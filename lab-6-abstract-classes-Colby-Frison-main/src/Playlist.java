466742c1c8f6329917d4063577f251b7a07a1327
Colby-Frison <colbyfrison100@gmail.com>
2024-02-22 12:56:55 -0600

    lab 6

--- lab-6-abstract-classes-Colby-Frison-main/src/Playlist.java
+++ lab-6-abstract-classes-Colby-Frison-main/src/Playlist.java
@@ -1,11 +1,42 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Playlist {
 	
 	private ArrayList<Song> songs;
 	
-	public Playlist() {
-		// TODO: Initialize the songs field.
+	public Playlist(String filename) {
+		String line = null;
+		BufferedReader reader;
+
+		ArrayList<Song> songTemps = new ArrayList<Song>();
+
+		try{
+			reader = new BufferedReader(new FileReader(filename));
+			line = reader.readLine();
+
+			while(line != null){
+				songTemps.add(new Song(line));
+	
+				line = reader.readLine();
+			}
+	
+			songs = songTemps;
+			
+			reader.close();
+		}
+		catch(IOException e){
+			System.out.println("no work");
+		}
+	}
+
+	public Playlist(){
+		songs = new ArrayList<Song>();
 	}
 	
 	public int getNumSongs() {
@@ -28,18 +59,72 @@ public class Playlist {
 	}
 	
 	public boolean addSong(int index, Song song) {
-		// TODO: Update the Lab 3 method.
+        //make sure its a valid mehtod call
+        if( index < 0 || index > songs.size() || song == null) {
+            return false;
+		}
+        
+        songs.add(index, song);
+        
+        return true;
 	}
 	
 	public int addSongs(Playlist playlist) {
-		// TODO: Update the Lab 3 method.
+        if(playlist == null){
+            return 0;
+		}
+
+		int cnt = 0;
+        for(Song song : playlist.getSongs()){
+			if(addSong(song))
+            	cnt++;
+        }
+        return cnt;
 	}
+
+	public int addSongs(String filename) {
+        String line = null;
+		BufferedReader reader;
+
+		int num = 0;
+
+		try{
+			reader = new BufferedReader(new FileReader(filename));
+			line = reader.readLine();
+
+			while(line != null){
+				songs.add(new Song(line));
+				num++;
+	
+				line = reader.readLine();
+			}
+			
+			reader.close();
+		}
+		catch(IOException e){
+			System.out.println("no work");
+		}
+
+		return num;
+	}
+
+
 	
 	public Song removeSong() {
 		return removeSong(getNumSongs() - 1);
 	}
 	
 	public Song removeSong(int index) {
-		// TODO: Update the Lab 3 method.
+
+		if(!(index < 0 || index >= getNumSongs())){
+			Song temp = songs.get(index);
+
+			songs.remove(index);
+
+			return temp;
+		}
+		else{
+			return null;
+		}
 	}
 }
--- lab-6-abstract-classes-Colby-Frison-main/src/Song.java
+++ lab-6-abstract-classes-Colby-Frison-main/src/Song.java
@@ -1,15 +1,42 @@
 import java.util.Arrays;
+import java.util.Collections;
+
 
 public class Song {
 	
 	private String title;
 	private String artist;
 	private int[] time;
+
+	private static final String INFO_DELIMITER = "; ";
+	private static final String TIME_DELIMITER = ":";
+
+	private static final int IDX_TITLE = 0;
+	private static final int IDX_ARTIST = 1;
+	private static final int IDX_TIME = 2;
+
 	
 	public Song(String title, String artist, int[] time) {
 		this.title = title;
 		this.artist = artist;
 		this.time = Arrays.copyOf(time, time.length);
+	}
+
+	public Song(String info){
+		String[] info2 = info.split(INFO_DELIMITER);
+		title = info2[0];
+		artist = info2[1];
+		String[] timeInfo = info2[2].split(TIME_DELIMITER);
+
+		Collections.reverse(Arrays.asList(timeInfo));
+
+		int[] temp = new int[timeInfo.length];
+
+		for(int i = 0; i < timeInfo.length; i++){
+			temp[i] = Integer.parseInt(timeInfo[i]);
+		}
+
+		time = temp;
 	}
 	
 	public String getTitle() {
@@ -22,5 +49,22 @@ public class Song {
 	
 	public int[] getTime() {
 		return Arrays.copyOf(time, time.length);
+	}
+
+	@Override
+	public String toString(){
+		String timeStr = "";
+		if(time.length == 3){
+			String form = String.format("%d:%02d:%02d", time[2], time[1], time[0]);
+			return "" + title + "; " + artist + "; " + form;
+		}
+		else if(time.length == 2){
+			String form = String.format("%d:%02d", time[1], time[0]);
+			return "" + title + "; " + artist + "; " + form;
+		}
+		else{
+			String form = String.format("%02d", time[0]);
+			return "" + title + "; " + artist + "; " + form;
+		}
 	}
 }
